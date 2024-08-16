c6707777d012d522407501c827172461b4380924
AJIN JIJI GEORGE <36074220+AJINJIJI@users.noreply.github.com>
2024-08-16 16:59:23 +0530

    Project completed

--- data_collection_form/appcollection/urls.py
+++ data_collection_form/appcollection/urls.py
@@ -3,5 +3,6 @@ from.import views
 
 urlpatterns = [
     path("",views.index,name="index"),
+    path("data",views.data,name="data"),
     
 ]
--- data_collection_form/appcollection/views.py
+++ data_collection_form/appcollection/views.py
@@ -5,3 +5,16 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request,"index.html")
+
+def data(request):
+    student_name=request.POST['a']
+    student_age=request.POST['b']
+    student_marks=request.POST['c']
+    subject=request.POST['d']
+    
+    
+    if int(student_age) >=18 and int(student_marks) >=40:
+        return render (request,"index.html",{"content1":"Eligible"})
+   
+    elif int(student_age) <=18 and int(student_marks) <=40 :
+        return render (request,"index.html",{"content2":" Not Eligible"})
